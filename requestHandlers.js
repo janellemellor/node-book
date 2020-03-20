69ed0f3e590c2eb4e913fc9bba75f4b8f746ae85
janelle mellor <janellemellor@gmail.com>
2020-03-20 13:46:28 -0700

    complete with formidable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,17 @@
 const querystring = require('querystring');
     fs = require('fs');
+    formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
     console.log(`Request handler 'start' was called.`);
 
     const body = '<html>' +
         '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />' +
+        '<meta http-equiv="Content-Type" '+ 'content="text/html; charset=UTF-8" />' +
         '</head>'+
         '<body>'+
         '<form action="/upload" enctype="multipart/form-data" '+ 'method="post">'+
-        '<input type="file" name="upload">'+
+        '<input type="file" name="upload" multiple="multiple">'+
         '<input type="submit" value="Upload file" />'+
         '</form>'+
         '</body>'+
@@ -23,11 +23,24 @@ function start(response, postData) {
     }
     
 
-    function upload(response, postData) {
+    function upload(response, request) {
         console.log(`Request handler 'upload' was called.`);
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write("You've sent the text: "+ querystring.parse(postData).text);
-        response.end();
+
+        const form = new formidable.IncomingForm();
+        console.log('about to parse');
+        form.parse(request, function(error, fields, files) {
+            console.log('parsing done');
+
+            fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+                if(error) {
+                    fs.unlink('/tmp/test.png');
+                }
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write("received image:<br/>");
+            response.write("<img src='/show' />");
+            response.end();
+        });
 }
 
 function show(response) {
